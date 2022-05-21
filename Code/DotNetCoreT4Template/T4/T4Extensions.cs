82355bf449c6b0e8292eec455dc24ecc7d34fc7a
Jeff Lee <jeff.lee@redcypress.com.tw>
2022-05-21 11:15:18 +0800

    Update READMe.md

--- Code/DotNetCoreT4Template/T4/T4Extensions.cs
+++ Code/DotNetCoreT4Template/T4/T4Extensions.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 
-//namespace DotNetCoreT4Template.T4
-//{
-//    internal static class T4Extensions
-//    {
-//        public static MethodInfo GetMethod(this Type type, string method, params Type[] parameters)
-//        {
-//            return type.GetRuntimeMethod(method, parameters);
-//        }
-//    }
-//}
+namespace DotNetCoreT4Template.T4
+{
+    internal static class T4Extensions
+    {
+        public static MethodInfo GetMethod(this Type type, string method, params Type[] parameters)
+        {
+            return type.GetRuntimeMethod(method, parameters);
+        }
+    }
+}
 
 namespace System.CodeDom.Compiler
 {
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 
+# Reminders
+
+ - Need to make sure it is TextTemplatingFilePreprocessor, when adding a Runtime Text Template
+
+        <None Update="T4\AppSettingsT4.tt">
+            <Generator>TextTemplatingFilePreprocessor</Generator>
+            <LastGenOutput>AppSettingsT4.cs</LastGenOutput>
+        </None>
+
+
+- Add this to the first line in tt file 
+
+        <#@ template visibility="internal" #>
+
+# References
+- [Use T4 TextTemplatingFilePreprocessor in .NET Standard or PCL libraries](https://blog.rsuter.com/use-t4-texttemplatingfilepreprocessor-in-net-standard-or-pcl-libraries/)
